6c34a3d74ff7bcf6fe96f019e4c1ab59afd83a0a
PavelKaratkevich <p_korotkevitch@mail.ru>
2022-07-20 23:45:10 +0200

    implement updateAccount handler

--- api/account.go
+++ api/account.go
@@ -73,7 +73,7 @@ func (server *Server) listAccount(ctx *gin.Context) {
 	}
 
 	arg := db.ListAccountParams{
-		Limit: req.PageSize,
+		Limit:  req.PageSize,
 		Offset: (req.PageID - 1) * req.PageSize,
 	}
 
@@ -84,7 +84,6 @@ func (server *Server) listAccount(ctx *gin.Context) {
 	}
 	ctx.JSON(http.StatusOK, accounts)
 }
-
 
 type deleteAccountRequest struct {
 	ID int64 `uri:"id" binding:"required,min=1"`
@@ -107,4 +106,33 @@ func (server *Server) deleteAccount(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(http.StatusOK, "the account has been successfully deleted")
+}
+
+type updateAccountRequest struct {
+	ID      int64 `json:"id" binding:"required,min=1"`
+	Balance int64 `json:"balance"`
+}
+
+func (server *Server) updateAccount(ctx *gin.Context) {
+	var req updateAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.UpdateAccountParams{
+		ID: req.ID,
+		Balance: req.Balance,
+	}
+
+	account, err := server.store.UpdateAccount(ctx, arg)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
 }
